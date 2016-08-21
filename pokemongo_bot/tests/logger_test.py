274bf75a4862da7dcd1af9bb7578f1a5dadd3c0c
Philipp Heuer <philipp.heuer@outlook.com>
2016-08-21 14:09:18 +0200

    Fixed UnitTest Detection

--- pokemongo_bot/tests/logger_test.py
+++ pokemongo_bot/tests/logger_test.py
@@ -10,7 +10,20 @@ from pokemongo_bot import Logger
 
 class LoggerTest(unittest.TestCase):
     @staticmethod
-    def test_log_by_call():
+    def test_debug_by_call():
+        import sys
+        from io import StringIO
+        out = StringIO()
+        sys.stdout = out
+
+        logger = Logger().getLogger('test')
+
+        logger.debug("log row", color="yellow")
+        output = out.getvalue().strip()
+        assert "[test] log row" in output
+
+    @staticmethod
+    def test_info_by_call():
         import sys
         from io import StringIO
         out = StringIO()
@@ -19,5 +32,44 @@ class LoggerTest(unittest.TestCase):
         logger = Logger().getLogger('test')
 
         logger.info("log row", color="yellow")
+        output = out.getvalue().strip()
+        assert "[test] log row" in output
+
+    @staticmethod
+    def test_warning_by_call():
+        import sys
+        from io import StringIO
+        out = StringIO()
+        sys.stdout = out
+
+        logger = Logger().getLogger('test')
+
+        logger.warning("log row", color="yellow")
+        output = out.getvalue().strip()
+        assert "[test] log row" in output
+
+    @staticmethod
+    def test_error_by_call():
+        import sys
+        from io import StringIO
+        out = StringIO()
+        sys.stdout = out
+
+        logger = Logger().getLogger('test')
+
+        logger.error("log row", color="yellow")
+        output = out.getvalue().strip()
+        assert "[test] log row" in output
+
+    @staticmethod
+    def test_critical_by_call():
+        import sys
+        from io import StringIO
+        out = StringIO()
+        sys.stdout = out
+
+        logger = Logger().getLogger('test')
+
+        logger.critical("log row", color="yellow")
         output = out.getvalue().strip()
         assert "[test] log row" in output
